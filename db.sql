8e6bf830741231cc046d273b3f80cc05bd2c4e3c
sauzzon <saujan.tiwari@gmail.com>
2022-11-25 17:36:47 +0545

    relational database design

--- db.sql
+++ db.sql
@@ -11,3 +11,27 @@ created_at timestamp without time zone NOT NULL
 DEFAULT (now() at time zone 'utc'),
 CHECK (lower(username) = username)
 );
+
+CREATE TABLE azdev.tasks (
+id serial PRIMARY KEY,
+content text NOT NULL,
+tags text,
+user_id integer NOT NULL,
+is_private boolean NOT NULL DEFAULT FALSE,
+approach_count integer NOT NULL DEFAULT 0,
+created_at timestamp without time zone NOT NULL
+DEFAULT (now() at time zone 'utc'),
+FOREIGN KEY (user_id) REFERENCES azdev.users
+);
+
+CREATE TABLE azdev.approaches (
+id serial PRIMARY KEY,
+content text NOT NULL,
+user_id integer NOT NULL,
+task_id integer NOT NULL,
+vote_count integer NOT NULL DEFAULT 0,
+created_at timestamp without time zone NOT NULL
+DEFAULT (now() at time zone 'utc'),
+FOREIGN KEY (user_id) REFERENCES azdev.users,
+FOREIGN KEY (task_id) REFERENCES azdev.tasks
+);
