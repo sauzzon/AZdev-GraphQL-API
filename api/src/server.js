5d87126453a550f434075ddddc2dfcb581bf19e4
sauzzon <saujan.tiwari@gmail.com>
2022-11-26 20:53:21 +0545

    communicating over http

--- api/src/server.js
+++ api/src/server.js
@@ -1,35 +1,30 @@
-/** GIA NOTES
- *
- * Use the code below to start a bare-bone express web server
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-import * as config from './config';
+import { graphqlHTTP } from "express-graphql";
+import { schema, rootValue } from "./schema";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import * as config from "./config";
+
 async function main() {
   const server = express();
   server.use(cors());
-  server.use(morgan('dev'));
+  server.use(morgan("dev"));
   server.use(bodyParser.urlencoded({ extended: false }));
   server.use(bodyParser.json());
-  server.use('/:fav.ico', (req, res) => res.sendStatus(204));
-  // Example route
-  server.use('/', (req, res) => {
-    res.send('Hello World');
-  });
-  // This line rus the server
+  server.use("/:fav.ico", (req, res) => res.sendStatus(204));
+
+  server.use(
+    "/",
+    graphqlHTTP({
+      schema,
+      rootValue,
+      graphiql: true,
+    })
+  );
+
   server.listen(config.port, () => {
     console.log(`Server URL: http://localhost:${config.port}/`);
   });
 }
 main();
-*/
-
-import { graphql } from "graphql";
-import { rootValue, schema } from "./schema";
-
-const executeGraphQLRequest = async (request) => {
-  const resp = await graphql({ schema, source: request, rootValue });
-  console.log(resp.data);
-};
-executeGraphQLRequest(process.argv[2]);
