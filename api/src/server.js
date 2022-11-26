a519de2c93069d84f6a776d79458181533d98ff7
sauzzon <saujan.tiwari@gmail.com>
2022-11-26 12:18:07 +0545

    graphql runtime setup

--- api/src/server.js
+++ api/src/server.js
@@ -24,3 +24,12 @@ async function main() {
 }
 main();
 */
+
+import { graphql } from "graphql";
+import { rootValue, schema } from "./schema";
+
+const executeGraphQLRequest = async (request) => {
+  const resp = await graphql({ schema, source: request, rootValue });
+  console.log(resp.data);
+};
+executeGraphQLRequest(process.argv[2]);
