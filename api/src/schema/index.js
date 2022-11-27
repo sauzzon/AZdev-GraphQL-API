86fd5a25dc1c32a040cdc17d60384a6e31432dd6
sauzzon <saujan.tiwari@gmail.com>
2022-11-27 19:27:43 +0545

    Revert "working with asynchronous functions"
    This reverts commit 5cfec4243c30c21a004aed1225575eb4e3d0e690.

--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -18,12 +18,8 @@ const QueryType = new GraphQLObjectType({
       type: GraphQLString,
       description: "The current time in ISO UTC",
       resolve: () => {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            const isoString = new Date().toISOString();
-            resolve(isoString.slice(11, 19));
-          }, 5000);
-        });
+        const isoString = new Date().toISOString();
+        return isoString.slice(11, 19);
       },
     },
     numbersInRange: {
