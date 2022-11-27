118f8a1e595f3ed7c0007b2d14608213c4ebbfe1
sauzzon <saujan.tiwari@gmail.com>
2022-11-27 17:45:51 +0545

    custom errors

--- api/src/utils.js
+++ api/src/utils.js
@@ -4,6 +4,10 @@ export const randomString = (bytesSize = 32) =>
   crypto.randomBytes(bytesSize).toString("hex");
 
 export const numbersInRangeObject = (begin, end) => {
+  if (end < begin) {
+    throw Error(`Invalid range because ${end}<${begin}`);
+  }
+
   let sum = 0;
   let count = 0;
   for (let i = begin; i <= end; i++) {
