40a9400e1061550e6d506c6a45ff0787758f1cee
sauzzon <saujan.tiwari@gmail.com>
2022-11-30 20:58:33 +0545

    circular dependency using function signature for fields

--- api/src/schema/types/approach.js
+++ api/src/schema/types/approach.js
@@ -7,10 +7,11 @@ import {
 } from "graphql";
 
 import User from "./user";
+import Task from "./task";
 
 const Approach = new GraphQLObjectType({
   name: "Approach",
-  fields: {
+  fields: () => ({
     id: { type: new GraphQLNonNull(GraphQLID) },
     content: { type: new GraphQLNonNull(GraphQLString) },
     voteCount: { type: new GraphQLNonNull(GraphQLInt) },
@@ -22,7 +23,11 @@ const Approach = new GraphQLObjectType({
       type: new GraphQLNonNull(User),
       resolve: (source, args, { loaders }) => loaders.users.load(source.userId),
     },
-  },
+    task: {
+      type: new GraphQLNonNull(Task),
+      resolve: (source, args, { loaders }) => loaders.tasks.load(source.taskId),
+    },
+  }),
 });
 
 export default Approach;
