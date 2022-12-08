2094f14e37694aa6196a733d35443915311fa37f
sauzzon <saujan.tiwari@gmail.com>
2022-12-08 12:49:29 +0545

    mutation type created

--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -1,6 +1,8 @@
 import { GraphQLSchema, printSchema } from "graphql";
 import QueryType from "./queries";
+import MutationType from "./mutations";
 export const schema = new GraphQLSchema({
   query: QueryType,
+  mutation: MutationType,
 });
 console.log(printSchema(schema));
