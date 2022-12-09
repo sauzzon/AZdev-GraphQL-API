1acdb9265b65225480388be616370b4607523fba
sauzzon <saujan.tiwari@gmail.com>
2022-12-09 11:46:23 +0545

    user delete mutation done

--- api/src/db/pg-api.js
+++ api/src/db/pg-api.js
@@ -186,6 +186,20 @@ const pgApiWrapper = async () => {
         }
         return payload;
       },
+      userDelete: async ({ currentUser }) => {
+        const payload = { errors: [] };
+        try {
+          await pgQuery(sqls.userDelete, {
+            $1: currentUser.id,
+          });
+          payload.deletedUserId = currentUser.id;
+        } catch (err) {
+          payload.errors.push({
+            message: "We were not able to delete this account",
+          });
+        }
+        return payload;
+      },
     },
   };
 };
