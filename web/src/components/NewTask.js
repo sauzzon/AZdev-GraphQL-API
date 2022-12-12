109cfc18bc2ed94ddd4c55abd1ee23a7dc6442a2
sauzzon <saujan.tiwari@gmail.com>
2022-12-12 21:46:47 +0545

    task create done

--- web/src/components/NewTask.js
+++ web/src/components/NewTask.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useStore } from '../store';
-import Errors from './Errors';
+import { useStore } from "../store";
+import Errors from "./Errors";
 
-/** GIA NOTES
- * Define GraphQL operations here...
- */
+const TASK_CREATE = `
+  mutation taskCreate($input: TaskInput! ) {
+    taskCreate(input: $input) {
+      errors {
+        message
+      }
+      task {
+        id
+      }
+    }
+  }
+`;
 
 export default function NewTask() {
-  const {
-    useLocalAppState,
-    setLocalAppState,
-    request,
-    AppLink,
-  } = useStore();
+  const { useLocalAppState, setLocalAppState, request, AppLink } = useStore();
   const [uiErrors, setUIErrors] = useState([]);
 
-  const user = useLocalAppState('user');
+  const user = useLocalAppState("user");
 
   if (!user) {
     return (
       <div className="box">
-        <div className="center">
-          Please login to create a Task record
-        </div>
+        <div className="center">Please login to create a Task record</div>
       </div>
     );
   }
@@ -31,29 +33,30 @@ export default function NewTask() {
   const handleNewTaskSubmit = async (event) => {
     event.preventDefault();
     const input = event.target.elements;
-
-    /** GIA NOTES
-     *
-     * 1) Invoke the mutation create a new task:
-     *   - input.*.value has what a user types in an input box
-     *
-     * 2) Use the code below after that. It needs these variables:
-     *   - `errors` is an array of objects of type UserError
-     *   - `task` is the newly created Task object
-
-      if (errors.length > 0) {
-        return setUIErrors(errors);
-      }
-      setLocalAppState({
-        component: { name: 'TaskPage', props: { taskId: task.id } },
-      });
-
-    */
+    const { data, errors: rootErrors } = await request(TASK_CREATE, {
+      variables: {
+        input: {
+          content: input.content.value,
+          tags: input.tags.value.split(","),
+          isPrivate: input.private.checked,
+        },
+      },
+    });
+    if (rootErrors) {
+      return setUIErrors(rootErrors);
+    }
+    const { errors, task } = data.taskCreate;
+    if (errors.length > 0) {
+      return setUIErrors(errors);
+    }
+    setLocalAppState({
+      component: { name: "TaskPage", props: { taskId: task.id } },
+    });
   };
 
   return (
     <div className="main-container">
-      <AppLink to="Home">{'<'} Cancel</AppLink>
+      <AppLink to="Home">{"<"} Cancel</AppLink>
       <div className="box box-primary">
         <form method="POST" onSubmit={handleNewTaskSubmit}>
           <div className="form-entry">
@@ -80,8 +83,8 @@ export default function NewTask() {
 
           <div className="form-entry">
             <label>
-              <input type="checkbox" name="private" /> Make this a
-              private entry (only for your account)
+              <input type="checkbox" name="private" /> Make this a private entry
+              (only for your account)
             </label>
           </div>
           <Errors errors={uiErrors} />
