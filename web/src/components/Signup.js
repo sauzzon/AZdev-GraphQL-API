29d0dd5c7c66862f86ec7a9f46e74ed68dce4cb3
sauzzon <saujan.tiwari@gmail.com>
2022-12-12 19:12:39 +0545

    user create done

--- web/src/components/Signup.js
+++ web/src/components/Signup.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useStore } from '../store';
-import Errors from './Errors';
+import { useStore } from "../store";
+import Errors from "./Errors";
 
-/** GIA NOTES
- * Define GraphQL operations here...
- */
+const USER_CREATE = `
+  mutation userCreate($input: UserInput!) {
+    userCreate(input: $input) {
+      errors {
+        message
+      }
+      user {
+        id
+        username
+      }
+      authToken
+    }
+  }
+`;
 
 export default function Signup() {
   const { request, setLocalAppState } = useStore();
@@ -14,27 +25,28 @@ export default function Signup() {
     event.preventDefault();
     const input = event.target.elements;
     if (input.password.value !== input.confirmPassword.value) {
-      return setUIErrors([{ message: 'Password mismatch' }]);
+      return setUIErrors([{ message: "Password mismatch" }]);
     }
 
-    /** GIA NOTES
-     *
-     * 1) Invoke the mutation to create a user record:
-     * - input.*.value has what a user types in an input box
-     *
-     * 2) Use the code below after that. It needs these variables:
-     *   - `errors` is an array of objects of type UserError
-     *   - `user` is a user object response from the API
-     *   - `authToken` is a string value response from the API
+    const { data } = await request(USER_CREATE, {
+      variables: {
+        input: {
+          firstName: input.firstName.value,
+          lastName: input.lastName.value,
+          username: input.username.value,
+          password: input.password.value,
+        },
+      },
+    });
 
-      if (errors.length > 0) {
-        return setUIErrors(errors);
-      }
-      user.authToken = authToken;
-      window.localStorage.setItem('azdev:user', JSON.stringify(user));
-      setLocalAppState({ user, component: { name: 'Home' } });
+    const { errors, user, authToken } = data.userCreate;
 
-    */
+    if (errors.length > 0) {
+      return setUIErrors(errors);
+    }
+    user.authToken = authToken;
+    window.localStorage.setItem("azdev:user", JSON.stringify(user));
+    setLocalAppState({ user, component: { name: "Home" } });
   };
   return (
     <div className="sm-container">
@@ -70,11 +82,7 @@ export default function Signup() {
             <div className="form-entry">
               <label>
                 CONFIRM PASSWORD
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  required
-                />
+                <input type="password" name="confirmPassword" required />
               </label>
             </div>
           </div>
