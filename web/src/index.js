79105c7e0d31a59984874929ac79e1d62da1c16c
sauzzon <saujan.tiwari@gmail.com>
2022-12-14 21:35:20 +0545

    ApolloProvider used

--- web/src/index.js
+++ web/src/index.js
@@ -1,17 +1,20 @@
-import 'regenerator-runtime/runtime';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "regenerator-runtime/runtime";
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
 
-import { useStoreObject, Provider } from './store';
-import Root from './components/Root';
+import { useStoreObject, Provider as StoreProvider } from "./store";
+import Root from "./components/Root";
 
 export default function App() {
   const store = useStoreObject();
   return (
-    <Provider value={store}>
-      <Root />
-    </Provider>
+    <ApolloProvider client={store.client}>
+      <StoreProvider value={store}>
+        <Root />
+      </StoreProvider>
+    </ApolloProvider>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
